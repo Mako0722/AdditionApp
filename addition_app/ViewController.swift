2c9ae6ccbec83311d32498b89ba77ad0601872c6
Mako0722 <zhongtiancheng79@gmail.com>
2022-11-30 14:43:47 +0900

    addition

--- addition_app/ViewController.swift
+++ addition_app/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    
+    
+    @IBOutlet weak var label: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func pressButton(_ sender: Any) {
+        let number1 = Int(textField1.text ?? "") ?? 0
+        let number2 = Int(textField2.text ?? "") ?? 0
+        
+        let result = number1 + number2
+        
+        label.text = "\(result)"
+    }
+    
 }
 
